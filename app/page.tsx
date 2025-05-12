6d36d66511940c12460e9154938033e7c274149c
Plachard-JT <michealsalam06@gmail.com>
2025-05-12 15:04:01 -0600

    fixed location error shown in main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { motion } from "framer-motion";
 import { Button } from "../components/ui/button";
 import { ChevronRight } from "lucide-react";
-import { Link } from "wouter";
+import Link from "next/link";
 
 export default function Home() {
   return (
